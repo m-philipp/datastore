36b590cc7923af2ced460042bb49ed1d16347229
Martin Zittel <martin.zittel@gmail.com>
2014-03-27 01:28:27 +0000

    added con timeout

--- telnetConnector/telnetServer.py
+++ telnetConnector/telnetServer.py
@@ -26,6 +26,20 @@ lock = threading.Lock()
 shutdown = 0
 
 
+# def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=5, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
 def handleSigTERM():
     shutdown = 1
 
@@ -84,6 +98,7 @@ class chatServer(threading.Thread):
         self.socket = socket
         self.address= address
         self.socket.settimeout(TIMEOUT)
+	set_keepalive_linux(self.socket)
 
     def run(self):
         lock.acquire()
